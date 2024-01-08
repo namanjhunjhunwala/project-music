3b4a9bfb0d7ab3fea3b58ab0fad0285841e9d0f1
namanjhunjhunwala <97025264+namanjhunjhunwala@users.noreply.github.com>
2024-01-08 20:52:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 song ="";
 function preload(){
     song = loadSound("music.mp3");
+    song = loadSound("music2.mp3")
 }
-song1 ="";
-function preload(){
-    song1 = loadSound("music2.mp3");
-}
+
+rightWristX = 0;
+rightWristY = 0;
+leftWristX = 0;
+leftWristY = 0;
 function setup(){
     canvas = createCanvas(600,500);
     canvas.center();
@@ -18,4 +20,20 @@ function draw(){
     image(video,0,0,600,500);
     fill("#FF0000");
     stroke("#FF0000");
+}
+function gotPoses(results)
+{
+     if(results.length > 0);
+     {
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreRightWrist = " + " scoreLeftWrist = " + scoreLeftWrist);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY =  " + rightWristY);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY =  " + leftWristY);
+        
+     }
 }
